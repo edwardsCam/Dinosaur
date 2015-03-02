0d3b567ebc20d18733154f4e046d2ae92a13ed47
Dwight <d.bennett45@outlook.com>
2015-03-02 16:40:31 -0700

    Opening Scene
    Added opening scene to game that load the world

---
+++ Assets/Scripts/PreGame/CameraRotation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour {
+
+    public Vector3 axis = new Vector3();
+    public float angle = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.Rotate(axis, angle*Time.deltaTime);
+	}
+}
