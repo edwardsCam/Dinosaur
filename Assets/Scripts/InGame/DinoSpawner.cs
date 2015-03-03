ab2d503659c20440866a0bcafa379af3b48819ca
Dwight <d.bennett45@outlook.com>
2015-03-02 17:41:37 -0700

    3 Different Dinosaurs
    Created 3 different dinosaurs and created a spawner

---
+++ Assets/Scripts/InGame/DinoSpawner.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Utilities;
+using Assets.Scripts;
+
+public class DinoSpawner : MonoBehaviour {
+
+    public GameObject tRex;
+    public GameObject smallDino;
+    public GameObject raptor;
+	// Use this for initialization
+	void Start () {
+        GameSettings settings = GameObject.FindGameObjectWithTag("Settings").GetComponent<GameSettings>();
+        SpawnDino(settings.GetCurrentDinosaur());
+
+        DestroySpawner();
+	
+	}
+	
+    private void DestroySpawner()
+    {
+        Destroy(gameObject, 3);
+    }
+	
+    private void SpawnDino(Dinosaur dino)
+    {
+        //I know this is bad code but I'm using it as a hotfix.
+        if(dino == Dinosaur.TRex)
+        {
+            Spawn(tRex);
+        }
+        else if (dino == Dinosaur.Small)
+        {
+            Spawn(smallDino);
+        }
+        else if (dino == Dinosaur.Raptor)
+        {
+            Spawn(raptor);
+        }
+    }
+
+    private void Spawn(GameObject dino)
+    {
+        GameObject instance = (GameObject)GameObject.Instantiate(dino, transform.position, transform.rotation);
+    }
+}
---
+++ Assets/Scripts/Utilities/GameSettings.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Utilities;
+
+namespace Assets.Scripts.Utilities
+{
+    public class GameSettings: MonoBehaviour
+    {
+        public Dinosaur currentDino;
+
+        void Awake()
+        {
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        public Dinosaur GetCurrentDinosaur()
+        {
+            return currentDino;
+        }
+
+        public void SetCurrentDinosaur(Dinosaur dino)
+        {
+            currentDino = dino;
+        }
+
+        
+    }
+}
