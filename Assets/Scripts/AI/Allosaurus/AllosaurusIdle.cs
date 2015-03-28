2382821c402fe711a24544ab8df1aabedcf87ab9
Dwight <d.bennett45@outlook.com>
2015-03-28 17:00:27 -0600

    Created a little AI framework
    I created a script that when attached it allows for the dinosaur to
    change his state.

---
+++ Assets/Scripts/AI/Allosaurus/AllosaurusIdle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.AI.Allosaurus
+{
+    class AllosaurusIdle : IDecision
+    {
+
+        public void Decide(GameObject self, GameObject target)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+            if (player != null)
+            {
+                if (Vector3.Distance(player.transform.position, self.transform.position) < 40)
+                {
+                    self.GetComponent<DinoAI>().UpdateDecision(new AllosaurusApproach());
+                }
+            }
+        }
+
+        public void Act(GameObject self, GameObject target)
+        {
+            Animation ani = self.GetComponent<Animation>();
+            ani.Play("Allosaurus_Idle");
+        }
+    }
+}
